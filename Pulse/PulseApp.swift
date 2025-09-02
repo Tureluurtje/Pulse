884ef62a98ebd0d8e6939ea6b2fa2458db6d5158
Tureluurtje <tureluurtje.1@gmail.com>
2025-09-02 21:02:27 +0200

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Arthur Kwak on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulseTests/PulseTests.swift
@@ -0,0 +1,16 @@
+//
+//  PulseTests.swift
+//  PulseTests
+//
+//  Created by Arthur Kwak on 9/2/25.
+//
+
+import Testing
+
+struct PulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
