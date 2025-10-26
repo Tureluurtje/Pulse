a0ccd79129f93960ff32e59c79cea6d78d598667
Tureluurtje <tureluurtje.1@gmail.com>
2025-10-26 11:37:06 +0100

    remove `replacements.txt` and update config.py

--- backend/config.py
+++ backend/config.py
@@ -36,7 +36,7 @@ DB: dict[str, Optional[str]] = {
 }
 
 # Auth / JWT settings
-SECRET_KEY: Optional[str] = require_env("***REMOVED***")
+SECRET_KEY: Optional[str] = require_env("SECRET_KEY")
 ALGORITHM: Optional[str] = require_env("ALGORITHM")
 
 _access_default: str = require_env("ACCESS_TOKEN_EXPIRE_MINUTES", "15")
