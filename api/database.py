841af8c6806760b05c20a3602914cd938d800758
Tureluurtje <tureluurtje.1@gmail.com>
2025-11-09 22:22:38 +0100

    fix: update config by adding DB_HOST & DB_PORT

--- api/database.py
+++ api/database.py
@@ -1,9 +1,7 @@
-from .config import DB
+from .config import DATABASE_URL
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = f"postgresql://{DB['USERNAME']}:{DB['PASSWORD']}@localhost:5432/{DB['DATABASE']}"
 
 engine = create_engine(url=DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
